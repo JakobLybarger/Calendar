731a41e2e409fdb4cbc6a7b40693c48af8fe49bb
Jakob Lybarger <jakoblybarger911@gmail.com>
2020-10-23 15:10:40 -0500

    Alert dialog to add events to [date]

--- lib/ui/calendar.dart
+++ lib/ui/calendar.dart
@@ -7,39 +7,53 @@ class Calendar extends StatefulWidget {
 }
 
 class _CalendarState extends State<Calendar> {
+  Map<DateTime, List<dynamic>> _events; // Map of each date and the events that day.
+  List<dynamic> _addedEvents; // List of events to be added.
+
   // Controller required to update events, holidays, etc.
   CalendarController _calendarController;
+  TextEditingController _eventController;
 
-  // Overridden to make initialize the calendar controller upon initialization
+
+  // Overridden to make initialize the controllers.
   @override
   void initState() {
     super.initState();
     _calendarController = CalendarController();
+    _eventController = TextEditingController();
+    _events = {};
+    _addedEvents = [];
   }
 
-  // Overridden to ensure dispose of calendar controller
+  // Overridden to ensure dispose of controllers.
   @override
   void dispose() {
     super.dispose();
     _calendarController.dispose();
+    _eventController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: SafeArea(
+    return Scaffold(
+      body: SafeArea(
         child: Container(
           child: Column(
             children: <Widget>[
               _buildCalendar(),
-              _eventList(),
             ],
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.deepPurpleAccent,
+        onPressed: _addEventDialog,
+      ),
     );
   }
 
+  // The calendar widget and its desired features/design.
   Widget _buildCalendar() {
     return TableCalendar(
       calendarController: _calendarController,
@@ -52,31 +66,40 @@ class _CalendarState extends State<Calendar> {
     );
   }
 
-  Widget _eventList() {
-    return ListView(
-      children: [
-        ListTile(
-          title: Text('monday'),
+  // Dialog to allow user to enter event for date.
+   _addEventDialog() async {
+    await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: _eventController,
         ),
-        ListTile(
-          title: Text('Tuesday'),
-        ),
-        ListTile(
-          title: Text('Wednesday'),
-        ),
-        ListTile(
-          title: Text('Thursday'),
-        ),
-        ListTile(
-          title: Text('Friday'),
-        ),
-        ListTile(
-          title: Text('Saturday'),
-        ),
-        ListTile(
-          title: Text('Sunday'),
-        ),
-      ],
+        actions: [
+          FlatButton(
+            child: Text("Add"),
+            onPressed: () {
+              // If there is not text return and do not add anything to events.
+              if(_eventController.text.isEmpty) return;
+
+              // If the day selected is not in _addedEvents, add it to _addedEvents and add the event.
+              if(_events[_calendarController.selectedDay] != null) {
+                _events[_calendarController.selectedDay].add(_eventController.text);
+              }
+              else { // Add the event to _addedEvents.
+                _events[_calendarController.selectedDay] = [_eventController.text];
+              }
+              // Clear controller and close alert dialog.
+              _eventController.clear();
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
     );
+
+    // Set _addedEvents equal to _events at selected day.
+    setState(() {
+      _addedEvents = _events[_calendarController.selectedDay];
+    });
   }
 }
