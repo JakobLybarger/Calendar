8b0658f53167e094665f2daf353ef76e997db7cd
Jakob Lybarger <jakoblybarger911@gmail.com>
2020-10-31 18:29:13 -0500

    Help page added with basic info

--- lib/ui/help.dart
+++ lib/ui/help.dart
@@ -5,8 +5,58 @@ class Help extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Help"),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent,
+        title: Text("Help"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(4.0),
+        child: Column(
+          children: [
+            Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: ListTile(
+                title: Text(
+                  "Add an Event",
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width / 15,
+                  ),
+                ),
+                subtitle: Text(
+                  "Tap on the day you want and click the Add button in the bottom right corner of the screen then enter event title.",
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width / 20,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: ListTile(
+                title: Text(
+                  "Delete an Event",
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width / 15,
+                  ),
+                ),
+                subtitle: Text(
+                  "Go to the date of the event and long press on the event and it will disappear",
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width / 20,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
